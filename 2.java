32875a8b2b4ee4520512f3fe961af90104385ffd
Satyam <satyamdev0525@gmail.com>
2018-08-30 08:20:27 +0530

    commit for merge

--- 2.java
+++ 2.java
@@ -1,7 +1,11 @@
+
+
 Hi , I have created my git account and i have started practice on it.
 And i know oneday i will be expert in it with in 5 days.
 Now I have created a branch and i am in win branch.
 
 Branches are created for seperate development if there is any bug or issues are arises. Because a developer can never start fixing the issue in same master branch.
 He need to create the seperate branch.
+
+this is a part of the merge confilct , so i m editing the file in this also.
 
