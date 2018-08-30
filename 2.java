1f99ce7f734b17390c027a3a5b238fc3fc249887
Satyam <satyamdev0525@gmail.com>
2018-08-30 08:18:44 +0530

    generating the merge conflict

--- 2.java
+++ 2.java
@@ -4,4 +4,18 @@ Now I have created a branch and i am in win branch.
 
 Branches are created for seperate development if there is any bug or issues are arises. Because a developer can never start fixing the issue in same master branch.
 He need to create the seperate branch.
+Now i m going to create the the merge conflict in the branches. so u will think how.
+
+so merge conflickt will occur when both the branches has different commit id for single file change.
+that is there is no common base commit for that file .
+
+for example in master branch we have a file 2. java
+i m goin to modify it and commit it.
+
+now the same file is in branch win also
+and i am going to do some chanegs there also
+and committed it.
+
+now when u will try to merge the win branch with master branch.
+it will give failure.
 
