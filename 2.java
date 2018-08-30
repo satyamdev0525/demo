5cc484f5f72b233bada3523e882aff9a55b80c99
Satyam <satyamdev0525@gmail.com>
2018-08-30 07:43:38 +0530

    commited from win branch

--- 2.java
+++ 2.java
@@ -1,3 +1,7 @@
 Hi , I have created my git account and i have started practice on it.
 And i know oneday i will be expert in it with in 5 days.
+Now I have created a branch and i am in win branch.
+
+Branches are created for seperate development if there is any bug or issues are arises. Because a developer can never start fixing the issue in same master branch.
+He need to create the seperate branch.
 
